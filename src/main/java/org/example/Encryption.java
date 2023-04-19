d1d5008ea09f2cbfe61cb392bdf5da77f33b9ead
Mridul <mridulbhawa@gmail.com>
2023-04-19 20:02:21 +0530

    completed the code in Encryption.java

--- src/main/java/org/example/Encryption.java
+++ src/main/java/org/example/Encryption.java
@@ -5,5 +5,30 @@
  */
 package org.example;
 
+import java.util.Scanner;
+
 public class Encryption {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Input the string : ");
+        String input= scanner.next();
+        Encryption.encryption(input);
+    }
+    public static void encryption(String input) {
+        char[] charArray = input.toCharArray();
+        for (char c : charArray) {
+            int temp= (int)c+4;
+            if (Character.isUpperCase(c)) {
+                if (temp>90){
+                    temp=temp-90+64;
+                }
+            } else if (Character.isLowerCase(c)) {
+                if (temp>122){
+                    temp=temp-122+97;
+                }
+            }
+            c=(char)temp;
+            System.out.print(c);
+        }
+    }
 }
